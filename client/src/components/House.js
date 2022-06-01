53da89552759a73d73a7022978c44328559eb102
Michael Franco <franco.luis.michael@gmail.com>
2022-06-01 08:55:41 -0500

    new change

--- client/src/components/House.js
+++ client/src/components/House.js
@@ -1,31 +1,24 @@
 import React from "react";
-import {NavLink} from 'react-router-dom'
-
 function House({ house }) {
   function handleBooking() {
 
-    // fetch
   }
-
   return (
     <div className="cards__item">
-      <div className="card">
+      <div className="cards">
         <img src={house.house_url} alt={house.name} className="image" />
         <div className="card__content">
-          <div className="card__title">{house.name}</div>
+          <div className="card-title">{house.name}</div>
           <div className="card__detail">
-          <p class="category">{house.address}</p>
-            <p class="category">Pet: {house.pet}</p>
-            <NavLink exact to="/BookingsPage"><button onClick={handleBooking}>Book Now!</button></NavLink>
-            <NavLink exact to='/houses/{house.id}'><button onClick={handleBooking}>Broken!</button></NavLink> 
-            {/* <button onClick={handleBooking}>Book Now!</button> */}
+            <p className="house-address">{house.address}</p>
+            <p className="house-pet">Pet: {house.pet}</p>
+            <button className="book-button" onClick={handleBooking}>Book Now!</button>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
 export default House;
 
 
@@ -33,3 +26,32 @@ export default House;
 // testing line 19 to handle use by id 
 
 // cannot figure out 
+
+// import React from "react";
+// import {NavLink} from 'react-router-dom'
+
+// function House({ house }) {
+//   function handleBooking() {
+
+//   }
+
+//   return (
+//     <div className="cards__item">
+//       <div className="card">
+//         <img src={house.house_url} alt={house.name} className="image" />
+//         <div className="card__content">
+//           <div className="card__title">{house.name}</div>
+//           <div className="card__detail">
+//           <p class="category">{house.address}</p>
+//             <p class="category">Pet: {house.pet}</p>
+//             <NavLink exact to="/BookingsPage"><button onClick={handleBooking}>Book Now!</button></NavLink>
+//             <NavLink exact to='/houses/{house.id}'><button onClick={handleBooking}>Broken!</button></NavLink> 
+//             {/* <button onClick={handleBooking}>Book Now!</button> */}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default House;
